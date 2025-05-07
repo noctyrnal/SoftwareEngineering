6c935f3306186a0b60828b33e3bf3891db48ee5b
Noctis <thbanham@gmail.com>
2025-05-07 20:28:09 +0100

    main schedule page

--- StaffScheduling/staffscheduling/urls.py
+++ StaffScheduling/staffscheduling/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from staffscheduling import views
 
 urlpatterns = [
-    path("", views.staff_scheduling, {"name": "Guest"}, name="staff_scheduling_default"),
-    path("<str:name>/", views.staff_scheduling, name="staff_scheduling"),
+    path("", views.staff_scheduling, {"name": "Guest"}, name="scheduling_page"),
+    path("time-off/", views.time_off_page, name="time_off_page"),
+    path("request-changes/", views.request_changes_page, name="request_changes_page"),
+    path("schedule-management/", views.schedule_management_page, name="schedule_management_page"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- StaffScheduling/staffscheduling/views.py
+++ StaffScheduling/staffscheduling/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
-import re
 from django.utils.timezone import datetime
-from django.http import HttpResponse
+import json
+import os
+from datetime import datetime as dt
 
 def staff_scheduling(request, name):
+    # Load the placeholder database from the JSON file
+    json_file_path = os.path.join(os.path.dirname(__file__), 'schedule_data.json')
+    with open(json_file_path, 'r') as file:
+        schedule_data = json.load(file)
+
+    # Add the day of the week dynamically based on the date
+    for entry in schedule_data:
+        entry_date = dt.strptime(entry["date"], "%d/%m/%Y")  # Parse the date
+        entry["day"] = entry_date.strftime("%A")  # Get the day of the week
+
     return render(
         request,
-        'staff_scheduling/staff_schedule.html',
+        'staff_scheduling/staff_schedule.html', # Request the static staff_schedule.htm file
         {
             'name': name,
-            'date': datetime.now()
+            'schedule_data': schedule_data, # Schedule data from schedule_data.json
         }
     )
+
+def schedule_page(request):
+    return render(request, 'staff_scheduling/schedule.html')
+
+def time_off_page(request):
+    return render(request, 'staff_scheduling/time_off.html')
+
+def request_changes_page(request):
+    return render(request, 'staff_scheduling/request_changes.html')
+
+def schedule_management_page(request):
+    return render(request, 'staff_scheduling/schedule_management.html')
